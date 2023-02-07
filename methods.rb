6ef521b66003291eb8fe6972aea514be279e1af1
lesayobruce <lesayobruce419@gmail.com>
2023-02-07 16:14:59 +0300

    completed test

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
-# Your code here!
+def greet_programmer(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_programmer()
+
+def greet(name = "friend")
+    puts "Hello, #{name}!"
+end
+greet()
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+
+def add(num1,num2)
+    return num1+num2
+end
+sum=add(2,5)
+ def halve(integer)
+   if integer.class == Integer
+    integer /2
+   else 
+    nil
+   end
+end
